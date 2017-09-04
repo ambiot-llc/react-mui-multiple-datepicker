64879677bbdce4b2bea45f4c3fb4541fb0020a21
Bilal Budhani <bilalbudhani@gmail.com>
2017-09-04 17:02:51 +0530

    Converts folder format to be library first & other housekeeping

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DatePicker from './lib';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DatePicker from "react-multiple-datepicker";
 
 class App extends Component {
   render() {
